bb3661ff3329e5ae5a324508a9482f62eb80b563
Christian LIM <christian.lim@outlook.fr>
2017-12-11 11:11:35 +0100

    Aveec le CSS valide

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'inscription', to: 'home#Index'
   get 'investir', to: 'pages#investir'
+  get 'about', to: 'pages#about'
   root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
