bcb4db3b6e48ae78654992d184ff9f2cfa00ee7e
Christian LIM <christian.lim@outlook.fr>
2017-12-12 11:38:51 +0100

    avec le bon domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://powerful-sierra-32593.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://young-crag-53838.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
